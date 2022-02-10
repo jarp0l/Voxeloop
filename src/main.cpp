1673aacb8bc4191019d244f063ce50439b570732
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2022-02-10 17:20:15 +0545

    Change shader paths

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,16 @@ int main() {
   gladLoaderLoadGL();
   glViewport(0, 0, 800, 800);
 
-  Shader shader("shaders/default.vert", "shaders/default.frag");
+  Shader shader("../src/shaders/default.vert", "../src/shaders/default.frag");
 
   // Vertices of triangle
   GLfloat vertices[] = {
-      -0.5f,     -0.5f * float(sqrt(3)) / 3,    0.0f,  // Lower left corner
-      0.5f,      -0.5f * float(sqrt(3)) / 3,    0.0f,  // Lower right corner
-      0.0f,      0.5f * float(sqrt(3)) * 2 / 3, 0.0f,  // Upper corner
-      -0.5f / 2, 0.5f * float(sqrt(3)) / 6,     0.0f,  // Inner left
-      0.5f / 2,  0.5f * float(sqrt(3)) / 6,     0.0f,  // Inner right
-      0.0f,      -0.5f * float(sqrt(3)) / 3,    0.0f   // Inner down
+      -0.5f,     -0.5f * float(sqrt(3)) / 3,    0.0f, // Lower left corner
+      0.5f,      -0.5f * float(sqrt(3)) / 3,    0.0f, // Lower right corner
+      0.0f,      0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+      -0.5f / 2, 0.5f * float(sqrt(3)) / 6,     0.0f, // Inner left
+      0.5f / 2,  0.5f * float(sqrt(3)) / 6,     0.0f, // Inner right
+      0.0f,      -0.5f * float(sqrt(3)) / 3,    0.0f  // Inner down
   };
 
   GLuint indices[] = {0, 3, 5, 3, 2, 4, 5, 4, 1};
