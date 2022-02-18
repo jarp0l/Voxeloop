8eb93e60a9066fbfe01941905ff3c2a00544bfb5
Rujal Acharya <076bei029,rujal@pcampus.edu.np>
2022-02-18 13:31:55 +0545

    Render Moving Cube

--- core/include/Main.hpp
+++ core/include/Main.hpp
@@ -6,6 +6,9 @@
 #include <GLFW/glfw3.h>
 // clang-format on
 #include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 #include "ebo.hpp"
